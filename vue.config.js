df27d979cfac38157b034dd71237ad6b4759885f
marek-onPC <mareksmieja@gmail.com>
2020-12-22 14:18:31 +0100

    Simple prerendering of main pages prepared

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,33 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/marek-onpc/'
-  : '/'
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Description of use: https://www.npmjs.com/package/prerender-spa-plugin
+        staticDir: path.join(__dirname, 'dist'),
+        routes: ['/', '/programming', '/baking', '/trips', '/about'],
+        captureAfterElementExists: '.v-card',
+        captureAfterTime: 5000,
+        postProcessHtml: function (context) {
+          var titles = {
+            '/': 'Home',
+            '/programming': 'Programming',
+            '/baking': 'Baking',
+            '/trips': 'Trips',
+            '/about': 'About'
+          }
+          return context.html.replace(
+            /<title>[^<]*<\/title>/i,
+            '<title>' + titles[context.route] + '</title>'
+          )
+        }
+      })
+    ],
+  }
 }
